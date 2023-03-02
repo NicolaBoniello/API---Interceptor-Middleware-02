c08add9c8fc9b342e750a63bc5d942fcad39b4ea
Nicola <nicolaboniello88@gmail.com>
2023-03-02 11:27:34 +0100

    fix controller

--- interceptors-middleware-2/src/main/java/co/develhope/interceptorsmiddleware2/controllers/BasicController.java
+++ interceptors-middleware-2/src/main/java/co/develhope/interceptorsmiddleware2/controllers/BasicController.java
@@ -1,9 +1,11 @@
 package co.develhope.interceptorsmiddleware2.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/default")
 public class BasicController {
 
     @GetMapping
